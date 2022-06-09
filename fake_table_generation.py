1abccdeff92650183efa39a93a72d89902d4fe9f
gkristaps <kristaps.garkevics@gmail.com>
2022-06-09 23:15:35 +0300

    Update fake_table_generation.py

--- fake_table_generation.py
+++ fake_table_generation.py
@@ -2,13 +2,14 @@ import random as r
 import datetime as dt
 from datetime import date as d
 from prettytable import PrettyTable as pt
+from random import *
 
 # table row count definition
-row_count = 1000
+row_count =  randint(1, 100) # can be fixed
 
-# date restrictions from 1st of January 2022 till Today
-r_start_date = dt.date(2022,1,1)
+# date restrictions 
 r_end_date = d.today()
+r_start_date = r_end_date - dt.timedelta(days = r.randrange(randint(1,100))) # can be fixed
 delta_days = r_end_date - r_start_date
 delta_days = delta_days.days # days between date range
 
@@ -19,16 +20,6 @@ email_subject_group = ['Purchase','Spam','Friend']
 table = pt()
 table.title = 'Fake Data'
 table.field_names = ['ID', 'Email Subject', 'Email Date']
-
-
-# one loop where each row have random date and email subject element
-# after generation added to the table 'Fake Data'
-# also, could create separete arrays of id, email_subject and email_date, and then add data to the prettytable
-# but one loop one interation created is more simpler, and, I suspect, better performance, as everything is done in one iteration. 
-# performance could be tested by changing the row count, and time() or  datetime.datetime.now() function on each step, 
-# and to see, which takes the most loading time
-# there are few libraries, which could help to check performance like timeit or cProfile, where one interation could be repeated multiple times
-# and peformance can be tested by changing variables, to generate more data, repeated multiple times. 
 
 for i in range(row_count):
     random_date = r_start_date + dt.timedelta(days = r.randrange(delta_days)) # add random number of days to 1st of January
