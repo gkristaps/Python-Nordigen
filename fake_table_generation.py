7887c484b426c48c8fdeb015827b62ac1dbfabe8
gkristaps <kristaps.garkevics@gmail.com>
2022-06-10 09:23:20 +0300

    Update fake_table_generation.py

--- fake_table_generation.py
+++ fake_table_generation.py
@@ -1,31 +1,36 @@
 import random as r
+import time as t
 import datetime as dt
 from datetime import date as d
 from prettytable import PrettyTable as pt
 from random import *
 
+
 # table row count definition
-row_count =  randint(1, 100) # can be fixed
+row_count =  randint(1, 100)
 
-# date restrictions 
+# date limitations to make data more realistic
 r_end_date = d.today()
-r_start_date = r_end_date - dt.timedelta(days = r.randrange(randint(1,100))) # can be fixed
-delta_days = r_end_date - r_start_date # can be changed, and used abs() to calc difference if r_end_date is random
-delta_days = delta_days.days # days between date range
+r_start_date = r_end_date - dt.timedelta(days = r.randrange(randint(10,100)))
 
-# email category names
+# function of random date 
+def random_date(x,y):
+    diff = abs((x - y).days)
+    random = r.randrange(diff)
+    return x + dt.timedelta(days = random)
+
+# array for email subject 
 email_subject_group = ['Purchase','Spam','Friend']
 
-# prettify fake data table
+# prettytable definition
 table = pt()
 table.title = 'Fake Data'
 table.field_names = ['ID', 'Email Subject', 'Email Date']
 
+# loop to generate rows
 for i in range(row_count):
-    random_date = r_start_date + dt.timedelta(days = r.randrange(delta_days)) # add random number of days
-    random_date = random_date.isoformat() # format date to isformat which is %Y-%m-%d
-# Email subject with random number in the end, number is in the range of 1 to delta_days
-    random_email_subject = 'Email Subject' + ' ' + r.choice(email_subject_group) + ' ' + str(r.randrange(delta_days))
-    table.add_row([i + 1,random_email_subject,random_date]) # adding new row with new values to the table
+    random_dates = random_date(r_start_date,r_end_date).isoformat()
+    random_email_subject = 'Email Subject' + ' ' + r.choice(email_subject_group) + ' ' + str(r.randrange(randint(1,100)))
+    table.add_row([i + 1,random_email_subject,random_dates])
 
 print(table)
